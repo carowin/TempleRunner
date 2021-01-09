833d1072f4345a00cf8090ca34158f7440c52315
Tarik Atlaoui <ta@pop-os.localdomain>
2021-01-09 12:11:32 +0100

    test

---
+++ TempleRunner-Android/app/src/main/java/com/example/TempleRunner_Android/GameLogic/Player.java
@@ -0,0 +1,28 @@
+package com.example.TempleRunner_Android.GameLogic;
+
+import android.graphics.Point;
+
+public class Player {
+    private Point position;
+    private PlayerStates state;
+    public Player(Point pos, PlayerStates state){
+        position=pos;
+        this.state=state;
+    }
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    public PlayerStates getState() {
+        return state;
+    }
+
+    public void setState(PlayerStates state) {
+        this.state = state;
+    }
+
+}
---
+++ TempleRunner-Android/app/src/main/java/com/example/TempleRunner_Android/GameLogic/Road.java
@@ -0,0 +1,15 @@
+package com.example.TempleRunner_Android.GameLogic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Road {
+    private List<Block> mainRoad = new ArrayList<>();
+    private List<Block> LeftRoad = null;
+    private List<Block> RightRoad = null;
+
+    public Road(){
+
+    }
+
+}
