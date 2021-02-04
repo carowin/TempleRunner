8f94884a7131d9d91919354c98415878c57519f0
carowin <carolinehuynh.h@gmail.com>
2021-02-04 19:43:57 +0100

    Ajout de sons + limite gauche droite du joueur

--- TempleRunner-IOS/TempleRunner-IOS/Models/Road.swift
+++ TempleRunner-IOS/TempleRunner-IOS/Models/Road.swift
@@ -278,6 +278,7 @@ class Road {
                 if elem.coin!.detectionCoin(player: player, screenOriginX : screenOriginX!) {
                     elem.coin?.removeCoin()
                     elem.changeCoinPresent(bool: false)
+                    view.activateCollectCoinSound()
                 }
             }
         }
