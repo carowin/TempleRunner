4f2ad6f7a12957198e1e9ea5a5cca0c5510bac7c
Radi Lina <lina.radi1996@gmail.com>
2021-02-19 00:01:50 +0100

    ajout de deux routes gauche et droite

--- TempleRunnerAndroid/app/src/main/java/com/sar/templerunner_android/GameLogic/Road.java
+++ TempleRunnerAndroid/app/src/main/java/com/sar/templerunner_android/GameLogic/Road.java
@@ -11,6 +11,7 @@ import java.util.Random;
 
 public class Road {
     private List<Block> mainRoad = new ArrayList<>();
+    private List<Block> sideRoad = new ArrayList<>();
     private List<Coin> coins = new ArrayList<>();
     /** size of one blocks */
     private int BlockSize;
@@ -18,9 +19,15 @@ public class Road {
     // a tester pour le découpage de l'ecran
     private final int diviseur = 10;
     private Random r = new Random();
+    private boolean done = false;
+    Resources res;
+    private int screenX, screenY;
     public Road(int screenX, int screenY , Resources res){
       //  Log.d("myTag", "X = " +screenX);
        // Log.d("myTag", "Y = " +screenY);
+        this.res = res;
+        this.screenX = screenX;
+        this.screenY = screenY;
         BlockSize = screenY /diviseur;
         int x = screenX/3;
         for (int pos =-BlockSize;pos<screenY;pos+=BlockSize)
@@ -30,8 +37,6 @@ public class Road {
                 mainRoad.add(new RoadRock(screenX/3,pos,BlockSize, screenY,res));
                 else
                 mainRoad.add(new branch(screenX/3,pos,BlockSize, screenY,res));
-
-
           for (int i= 0 ;i < NB_COIN ; i++){
             if(i%2 == 0)
                 coins.add(new SimpleCoin(x,0,BlockSize/4,screenY,res));
@@ -44,7 +49,27 @@ public class Road {
 
     public void upDateRoad(Canvas c){
        // Log.d("myTag", "Y = " +mainRoad.size());
-        for (Block b : mainRoad)
+        Log.d("int", ""+r.nextInt(100));
+        if (!done && r.nextInt(800) == 0){
+            //cree deux route gauche / droite
+            //mettre le boolean a true
+            done = true;
+            for (int i = 0 ; i < screenX/BlockSize + 1 ; i++) {
+                sideRoad.add(new SimpleRoad(i*BlockSize,-BlockSize,BlockSize, screenY,res));
+            }
+        }
+        if (done){
+            for (Block b : mainRoad){
+                if (b.y >= sideRoad.get(0).y)
+                    b.updatePosition(c);
+            }
+
+        } else {
+            for (Block b : mainRoad)
+                b.updatePosition(c);
+        }
+
+        for (Block b : sideRoad)
             b.updatePosition(c);
 
     }
